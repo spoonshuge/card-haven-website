23477df30d6bebfaa5073840c618f9b53ed5379f
spoon <daniel.rowland@gmail.com>
2025-05-23 22:00:32 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: mode === 'production' ? '/your-repo-name/' : '/',
+  base: mode === 'production' ? '/card-haven-website/' : '/',
   plugins: [
     react(),
     mode === 'development' &&
